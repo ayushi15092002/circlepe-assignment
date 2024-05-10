0c71d15399b31a56b3d7fa729f3776c87b69eafd
Ayushi Agarwal <ayushiagg1509@gmail.com>
2024-05-10 14:29:48 +0530

    Update index.js

--- Log-Ingestor-and-Query-Interface/Log-Ingestor/index.js
+++ Log-Ingestor-and-Query-Interface/Log-Ingestor/index.js
@@ -6,9 +6,19 @@ import logRouter from './features/log/log.routes.js';
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, *');
-  res.header('Access-Control-Allow-Methods', '*');
+  // res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
+  // res.header('Access-Control-Allow-Headers', 'Content-Type, *');
+  // res.header('Access-Control-Allow-Methods', '*');
+
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   if (req.method == 'OPTIONS') {
     return res.sendStatus(200);
   }
